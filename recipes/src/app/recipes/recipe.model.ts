a60e6555c6fd9414894f8b5ed4e8524d742cb487
henrytran721 <henryhtran07@gmail.com>
2021-03-31 15:12:35 -0700

    angular recipe project setup

---
+++ recipes/src/app/recipes/recipe.model.ts
@@ -0,0 +1,14 @@
+// define how a single recipe looks like (structure)
+// blue print for objects we create
+
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, description: string, imagePath: string) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+}
